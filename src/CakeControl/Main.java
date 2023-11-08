782a2cc8e8c224b177442fb3002ace88fcafd24c
c4dtu <c4dtu@DESKTOP-LM6IT5E>
2023-11-07 21:03:16 -0300

    Primera modificación branch Nicolás

--- src/CakeControl/Main.java
+++ src/CakeControl/Main.java
@@ -5,5 +5,7 @@ package CakeControl;
  * @author Nicolás Carmona y Eduardo Reyes
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Estamos probando los cambios");
+    }
 }
