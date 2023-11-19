8b8889a4603d3e7c34e559dec2863478d3b44e64
s-e-b <s-e-b@seba>
2023-11-19 11:53:21 -0300

    agregar ingredientes

--- src/Controladores/ControladorReceta.java
+++ src/Controladores/ControladorReceta.java
@@ -29,7 +29,23 @@ public class ControladorReceta {
         }
     }
     
-    
+    public void buscarReceta(){ //Método que busca un socio a partir del numrun de la persona
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cakecontrol","root", "");
+            String query = "SELECT nombre_receta,valor_receta FROM receta WHERE cod_receta = '"+receta.getCodReceta()+"'";
+            ResultSet rs = connection.createStatement().executeQuery(query);
+            if(rs.next()){
+                receta.setNombreReceta(rs.getString("nombre_receta"));
+                receta.setvalorReceta(Integer.parseInt(rs.getString("valor_receta")));
+                
+            }
+            
+        }catch (Exception e){
+            System.out.println("Algo salió mal: "+e);
+            JOptionPane.showMessageDialog(null, "Error al buscar receta");
+        }
+    }
     
     
 }
